dbbe16ee6e42174f8a4c8047005eac6f6e6d9fb2
James Hwang <james.hwang.usc@gmail.com>
2013-03-11 01:04:24 -0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApprenticeTracking::Application.config.secret_token = 'cde85da77b747d9307746a9c657f0ea57a3d966c27d0c22f33c4d9e7246c8f1512eb8dd76e424b7a27185316365b446c0c20bef37983f38792690dc98bd3b92e'
